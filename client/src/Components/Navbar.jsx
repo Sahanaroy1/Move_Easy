d1f7b70c6214b2681ba7bafcc7fcbd3b5f766008
Sahanaroy1 <sahanaroy20@gmail.com>
2024-06-27 21:01:21 +0100

    updated the frontend css with positioning

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -19,7 +19,7 @@ function Navigation() {
       <div className="logo-container">
       
         <h1>Move Easy</h1>
-        <img src={logo} className="logo"/>
+        
 
       </div>
       <ul className="nav-links-right">
