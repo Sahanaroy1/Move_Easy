9728ce616589cbfb359c82ef5facb68a8f64f84d
Sahanaroy1 <sahanaroy20@gmail.com>
2024-07-01 20:48:14 +0100

    added footer icons

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import '../styles/Footer.css';
+import facebook from '../assets/images/facebook.png'; 
+import insta from '../assets/images/insta.png';
+import youtube from '../assets/images/youtube.png';
+import tiktok from '../assets/images/tiktok.png';
+
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
+        <img src={facebook}/>
+        <img src={insta}/>
+        <img src={youtube}/>
+        <img src={tiktok}/>
         <p>@ MoveEasy2024</p>
       </div>
     </footer>
